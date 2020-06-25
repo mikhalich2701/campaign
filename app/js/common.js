9fadcc4ee4182e84abf6fd8d540e30f0c536bcdc
mikhalich2701 <mikhalich2701@yandex.ru>
2020-06-25 16:34:49 +0300

    campaign2

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,13 @@
-$(document).ready(function (){
-
-
-
+$(document).ready(function () {
+  $(".desctiption__nav").on("click", "li:not(.active)", function () {
+    $(this)
+      .addClass("active")
+      .siblings()
+      .removeClass("active")
+      .closest("div.desctiption")
+      .find("div.desctiption__content")
+      .removeClass("active")
+      .eq($(this).index())
+      .addClass("active");
+  });
 });
